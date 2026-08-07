33c60c34771e931653fd06d748672c6df3993cd0
Thiago Rodrigues <icrowerz@hotmail.com>
2026-08-06 22:23:54 -0300

    Implementando Login no Firebase

--- js/firebase.js
+++ js/firebase.js
@@ -1,33 +1,30 @@
+// Importa o Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 
+// Importa o Firestore (Banco de Dados)
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+// Importa o Authentication (Login)
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+// Configuração do seu projeto Firebase
 const firebaseConfig = {
-
   apiKey: "AIzaSyBqN9f11xl5-mxybU0KrCT4HgLi43F7PzI",
-
   authDomain: "projeto-social-d3f84.firebaseapp.com",
-
   projectId: "projeto-social-d3f84",
-
   storageBucket: "projeto-social-d3f84.firebasestorage.app",
-
   messagingSenderId: "670247263977",
-
   appId: "1:670247263977:web:4cde8c7bf4c0bb6c7d1a78"
-
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-
-import {
-
-getFirestore
-
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-
-const app = initializeApp(firebaseConfig);
-
+// Inicializa o Firestore
 const db = getFirestore(app);
 
-export { db };
+// Inicializa o Authentication
+const auth = getAuth(app);
+
+// Exporta para os outros arquivos
+export { db, auth };
